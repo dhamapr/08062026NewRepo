46fc3004de5d4228ae2a07de39e279b014eb921e
dhamapr <PrashantDhama2002@gmail.com>
2026-06-09 12:28:16 +0530

    Add files via upload

---
+++ FrameWorkDemoMaven2/src/test/java/BasedLibrary/BasedLibrary.java
@@ -0,0 +1,18 @@
+package BasedLibrary;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BasedLibrary {
+	
+	@Test
+	public void launchUrl() {
+		
+		
+		WebDriver driver = new ChromeDriver();
+	}
+
+}
